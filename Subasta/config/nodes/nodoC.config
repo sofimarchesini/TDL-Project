3f86664fd226c9b240a8f7f824de0e4a901e222f
Mateo Bulnes <mbulnes@fi.uba.ar>
2022-12-05 17:27:13 -0300

    Agregamos las configuraciones

---
+++ Subasta/config/nodes/nodoC.config
@@ -0,0 +1,5 @@
+[{kernel,
+	[{distributed, [{'iasc_tp_subastas', 5000, ['nodoA@localhost', {'nodoB@localhost', 'nodoC@localhost'}]}]},
+	 {sync_nodes_mandatory, ['nodoA@localhost', 'nodoB@localhost']},
+	 {sync_nodes_timeout, 30000}
+]}].
---
+++ Subasta/config/test.exs
@@ -0,0 +1,16 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :iasc_tp_subastas, IascTpSubastas.Endpoint,
+  http: [port: 4001],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :iasc_tp_subastas, IascTpSubastas.Repo,
+  adapter: Mongo.Ecto,
+  database: "iasc_tp_subastas_test",
+  pool_size: 1

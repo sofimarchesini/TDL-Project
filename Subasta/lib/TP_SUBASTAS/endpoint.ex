2c5ec33e707b6a91bb874330615c50dba81d65d3
Mateo Bulnes <mbulnes@fi.uba.ar>
2022-12-05 17:11:05 -0300

    Completamos funcionalidades

---
+++ Subasta/lib/TP_SUBASTAS/endpoint.ex
@@ -0,0 +1,44 @@
+defmodule IascTpSubastas.Endpoint do
+  use Phoenix.Endpoint, otp_app: :iasc_tp_subastas
+
+  socket("/socket", IascTpSubastas.UserSocket)
+
+  # Serve at "/" the static files from "priv/static" directory.
+  #
+  # You should set gzip to true if you are running phoenix.digest
+  # when deploying your static files in production.
+  plug(Plug.Static,
+    at: "/",
+    from: :iasc_tp_subastas,
+    gzip: false,
+    only: ~w(css fonts images js favicon.ico robots.txt)
+  )
+
+  # Code reloading can be explicitly enabled under the
+  # :code_reloader configuration of your endpoint.
+  if code_reloading? do
+    socket("/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket)
+    plug(Phoenix.LiveReloader)
+    plug(Phoenix.CodeReloader)
+  end
+
+  plug(Plug.RequestId)
+  plug(Plug.Logger)
+
+  plug(Plug.Parsers,
+    parsers: [:urlencoded, :multipart, :json],
+    pass: ["*/*"],
+    json_decoder: Poison
+  )
+
+  plug(Plug.MethodOverride)
+  plug(Plug.Head)
+
+  plug(Plug.Session,
+    store: :cookie,
+    key: "_iasc_tp_subastas_key",
+    signing_salt: "3fxCKsXc"
+  )
+
+  plug(IascTpSubastas.Router)
+end
---
+++ Subasta/lib/TP_SUBASTAS/repo.ex
@@ -0,0 +1,3 @@
+defmodule IascTpSubastas.Repo do
+  use Ecto.Repo, otp_app: :iasc_tp_subastas
+end
